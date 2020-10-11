c0ed2e2e7203df784ec18a714f5916daf44106eb
Noura Loudani <contact.nouraloudani@gmail.com>
2020-10-11 15:28:18 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,12 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def input_to_index(input)
+  return input.to_i - 1
+end
+
+def move(board, input, character = "X")
+  board[input] = character
+  return board
+end
